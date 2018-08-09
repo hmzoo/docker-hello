1a924b4c0460d50153e0361a9387b6bb218d886c
HMJ <hmj@goatpage.com>
2018-08-09 10:57:56 +0200

    ok

--- main.go
+++ main.go
@@ -14,5 +14,5 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintf(w, "Hello, %q\n", html.EscapeString(r.URL.Path))
 }
